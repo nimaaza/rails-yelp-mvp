0a4fe3c2375fff05312e027fee854c856f1632be
Nima Azarbayjani <nimaza@gmail.com>
2020-11-03 12:55:02 +0330

    added actions in the restaurant controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,26 @@
 class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def update
+    @restaurant = Restaurant.new(restaurat_params)
+    @restaurant.save!
+
+    redirect_to @restaurant
+  end
+
+  private
+
+  def restaurat_params
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
+  end
 end

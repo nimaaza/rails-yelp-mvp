d19968056e51684b202b7f9c81926e4ef4b45f90
Nima Azarbayjani <nimaza@gmail.com>
2020-11-03 12:22:04 +0330

    seeded the restaurants database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone_with_country_code,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+
+  restaurant.save
+end

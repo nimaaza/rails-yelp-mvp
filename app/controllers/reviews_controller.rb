194d2f8bd71f9bd974354686126d16b873c87236
Nima Azarbayjani <nimaza@gmail.com>
2020-11-03 13:14:06 +0330

    coded required actions in the reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,25 @@
 class ReviewsController < ApplicationController
+  def new
+    @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+
+    if @review.save
+      redirect_to @review.restaurant
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content, :restaurant_id)
+  end
 end
